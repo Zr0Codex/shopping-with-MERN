5cf44fec1ccd5ff30d96f7cd9aacd02e6b965737
tontrakan <73891377+Zr0Codex@users.noreply.github.com>
2022-11-04 16:01:14 +0700

    test deploy to server

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,31 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './router/productRoutes.js';
+import userRoutes from './router/userRoutes.js';
 import { notFound, errorHandler } from './errorMiddleware/errorMiddleware.js';
+import path from 'path';
 
 dotenv.config();
 connectDB();
 
 const app = express();
+const __dirname = path.resolve();
+
+app.use(express.json());
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
 
 app.use(notFound);
 
